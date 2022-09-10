d0fd42642a15bbe91a9c02d3502404b43ae2b35d
Gregory Wawszyniak Dumont <gregory.wd@pm.me>
2022-09-10 04:00:27 +0200

    Update Caesars_Cipher.js

--- JavaScript-Algorithms-and-Data-Structures/projects/Caesars_Cipher.js
+++ JavaScript-Algorithms-and-Data-Structures/projects/Caesars_Cipher.js
@@ -6,14 +6,27 @@ All letters will be uppercase. Do not transform any non-alphabetic character (i.
 
 function rot13(str) {
 
-    const alphaList = {
-        1: "A",
-        2: "B",
-        3: "C",
-        ...
+    // I want to split the character str.split('') into an array
+    let strUp = str.toUpperCase();
+    const arr = strUp.split('');
+
+    //Eiter I create an object or one of Charcode methods 
+
+    //I replace the character by adding 13, except if using charcode 
+    for (let i = 0; i > arr.length; i++) {
+        charValue = arr[i].charCodeAt();
+        if (charValue >= 'A'.charCodeAt() && charValue <= 'Z'.charCodeAt()) {
+            if (charValue <= 'N'.charCodeAt()) {
+                charChange = charValue + 13;
+            }
+            else {
+                charChange = charValue - 13;
+            }
+        }
+        result = String.fromCharCode(charChange);
     }
-  
-    return str;
+    //change the array into a string and return
+    return result.join('') ;
   }
   
   rot13("SERR PBQR PNZC");
