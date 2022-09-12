5ec5c77a2048329eef6af39f5921ade363440298
Gregory WD <gregory.wd@pm.me>
2022-09-12 17:50:55 +0200

    Create lookahead_regex.js

---
+++ JavaScript-Algorithms-and-Data-Structures/exercises/lookahead_regex.js
@@ -0,0 +1,7 @@
+/*A more practical use of lookaheads is to check two or more patterns in one string.
+
+Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.*/
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6,})(?=\D*\d{2})/; // Change this line
+let result = pwRegex.test(sampleWord);
