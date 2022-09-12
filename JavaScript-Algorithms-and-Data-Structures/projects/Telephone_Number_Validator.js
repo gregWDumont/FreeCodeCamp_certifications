b8a46cd9caef2d5479104e8623d573ad3f2150ce
Gregory WD <gregory.wd@pm.me>
2022-09-12 12:09:02 +0200

    Create Telephone_Number_Validator.js

---
+++ JavaScript-Algorithms-and-Data-Structures/projects/Telephone_Number_Validator.js
@@ -0,0 +1,19 @@
+function telephoneCheck(str) {
+
+    return str;
+  }
+  
+  telephoneCheck("555-555-5555");
+
+/* 
+
+The following are examples of valid formats for US numbers:
+
+    555-555-5555
+    (555)555-5555
+    (555) 555-5555
+    555 555 5555
+    5555555555
+    1 555 555 5555
+
+*/
