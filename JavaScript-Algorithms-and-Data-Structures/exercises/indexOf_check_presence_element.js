baf19fce119e316148052d3c4f0803be534a2fab
Gregory Wawszyniak Dumont <gregory.wd@pm.me>
2022-09-13 19:40:31 +0200

    Create indexOf_check_presence_element.js

---
+++ JavaScript-Algorithms-and-Data-Structures/exercises/indexOf_check_presence_element.js
@@ -0,0 +1,29 @@
+/* indexOf() can be incredibly useful for quickly checking for the presence of an element on an array. We have defined a function, quickCheck, that takes an array and an element as arguments. Modify the function using indexOf() so that it returns true if the passed element exists on the array, and false if it does not. */
+
+//my solution
+
+function quickCheck(arr, elem) {
+    // Only change code below this line
+  if (arr.indexOf(elem) > -1){
+    return true;
+    }
+  return false
+  
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// Solution 2
+
+function quickCheck(arr, elem) {
+    return arr.indexOf(elem) >= 0 ? true : false;
+  }
+  console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+
+// Solution 3
+
+function quickCheck(arr, elem) {
+    return arr.indexOf(elem) != -1;
+  }
+  console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
