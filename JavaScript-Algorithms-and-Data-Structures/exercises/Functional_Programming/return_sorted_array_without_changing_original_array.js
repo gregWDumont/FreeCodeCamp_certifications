20f01335084c9785c46668ce74a9b069a0ee5edb
Gregory WD <gregory.wd@pm.me>
2022-09-20 20:02:03 +0200

    Create return_sorted_array_without_changing_original_array.js

---
+++ JavaScript-Algorithms-and-Data-Structures/exercises/Functional_Programming/return_sorted_array_without_changing_original_array.js
@@ -0,0 +1,30 @@
+// Solution 1
+
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return arr
+  .slice(0)
+  .concat()
+  .sort(function(a, b) {
+    return a - b;
+  })
+
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
+
+
+// Solution 2
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Add your code below this line
+  return [].concat(arr).sort(function(a, b) {
+    return a - b;
+  });
+  // Add your code above this line
+}
+nonMutatingSort(globalArray);
