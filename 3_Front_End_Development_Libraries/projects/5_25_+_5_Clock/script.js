e8f013d947c251938ed1fd8b27ae23f47e7a6a64
Gregory WD <gregory.wd@pm.me>
2023-01-28 03:12:08 +0100

    Create project boilerplate

---
+++ 3_Front_End_Development_Libraries/projects/5_25_+_5_Clock/script.js
@@ -0,0 +1,14 @@
+function App() {
+
+
+    return (
+        <div className="container">
+            <h1>25 + 5 Clock</h1>
+        </div>    
+            
+    )
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
