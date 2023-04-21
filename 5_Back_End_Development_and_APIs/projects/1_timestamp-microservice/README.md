91cd4fd16c7ff8a190fc3d96953b8501e3c9ebdf
Gregory WD <gregory.wd@pm.me>
2023-04-22 00:24:30 +0200

    Add solution 1st proj Backend - Timestamp

---
+++ 5_Back_End_Development_and_APIs/projects/1_timestamp-microservice/README.md
@@ -0,0 +1,15 @@
+# Timestamp Microservice
+
+Build a full stack JavaScript app that is functionally similar to this: https://timestamp-microservice.freecodecamp.rocks.
+
+### Start project
+
+cmd: 'npm start'
+
+### Access API
+
+[link](http://localhost:4100/)
+
+### Use API
+
+http://localhost:4100/ + "your date"
---
+++ 5_Back_End_Development_and_APIs/projects/1_timestamp-microservice/index.js
@@ -0,0 +1,58 @@
+// index.js
+// where your node app starts
+
+// init project
+const express = require("express");
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+const getTimestamp = (date) => ({
+  unix: date.getTime(),
+  utc: date.toUTCString(),
+});
+
+// your API endpoint...
+app.get("/api/timestamp/:dateString?", (req, res) => {
+  const { dateString } = req.params;
+  let timestamp;
+
+  if (dateString === undefined || dateString.trim() === "") {
+    timestamp = getTimestamp(new Date());
+  } else {
+    const date = !isNaN(dateString)
+      ? new Date(parseInt(dateString))
+      : new Date(dateString);
+
+    if (!isNaN(date.getTime())) {
+      timestamp = getTimestamp(date);
+    } else {
+      timestamp = {
+        error: "invalid date",
+      };
+    }
+  }
+
+  res.json(timestamp);
+});
+
+// 404 page
+app.use((req, res) => {
+  	res.status(404).sendFile(__dirname + "/views/404.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT || 4100, () => {
+  	console.log(`Your app is listening on port ${listener.address().port}`);
+});
